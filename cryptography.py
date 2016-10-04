a7db600228aaa3e1a49f1d347b3ac6a83befb935
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-10-04 13:54:13 -0400

    Updated from Brython Server: 10/4/2016 1:54:14 PM

--- cryptography.py
+++ cryptography.py
@@ -24,5 +24,8 @@ if mode == "e":
         tochange[x]=associations.find(tochange[x])
     for x in range(0, len(key)):
         key[x]=associations.find(key[x])
-    print(tochange)
+    for x in range(0, len(tochange)):
+        y=x
+        if x>len(key):
+            
         
