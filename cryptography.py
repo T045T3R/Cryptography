671f8cce5484ba7052e28ac5addd0e9b5dd024da
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-10-04 13:24:07 -0400

    Updated from Brython Server: 10/4/2016 1:24:08 PM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Johannes Testorf
+Credit: Wilson Rimberg
 
 Assignment:
 
@@ -10,3 +10,9 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+
+mode = str(input("Enter e to encrypt, d to decrypt, or q to quit: "))
+while not mode in ["e","d","q"]:
+    mode = str(input("Enter e to encrypt, d to decrypt, or q to quit: "))
+if mode = q:
+    print("Goodbye!")
