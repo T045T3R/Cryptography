a4e4114134b7ab4f536451ae869e503125a3f2f0
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-10-04 14:23:31 -0400

    Updated from Brython Server: 10/4/2016 2:23:33 PM

--- cryptography.py
+++ cryptography.py
@@ -10,36 +10,38 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
-
-mode = str(input("Enter e to encrypt, d to decrypt, or q to quit: "))
-while not mode in ["e","d","q"]:
-    print("Did not understand command, try again.")
+a=0
+while a!=1:
     mode = str(input("Enter e to encrypt, d to decrypt, or q to quit: "))
-if mode == "q":
-    print("Goodbye!")
-if mode == "e":
-    tochange = list(input("Message: "))
-    key = list(input("Key: "))
-    for x in range(0, len(tochange)):
-        tochange[x]=associations.find(tochange[x])
-    for x in range(0, len(key)):
-        key[x]=associations.find(key[x])
-    for x in range(0, len(tochange)):
-        a=x//int(len(key))
-        y=int(x-(a*len(key)))
-        code= int(tochange[x]+key[y])
-        print(associations[code], end="")
-    print()
-if mode == "d":
-    tochange = list(input("Message: "))
-    key = list(input("Key: "))
-    for x in range(0, len(tochange)):
-        tochange[x]=associations.find(tochange[x])
-    for x in range(0, len(key)):
-        key[x]=associations.find(key[x])
-    for x in range(0, len(tochange)):
-        a=x//int(len(key))
-        y=int(x-(a*len(key)))
-        code= int(tochange[x]-key[y])
-        print(associations[code], end="")
-    print()
+    while not mode in ["e","d","q"]:
+        print("Did not understand command, try again.")
+        mode = str(input("Enter e to encrypt, d to decrypt, or q to quit: "))
+    if mode == "q":
+        print("Goodbye!")
+        a=1
+    if mode == "e":
+        tochange = list(input("Message: "))
+        key = list(input("Key: "))
+        for x in range(0, len(tochange)):
+            tochange[x]=associations.find(tochange[x])
+        for x in range(0, len(key)):
+            key[x]=associations.find(key[x])
+        for x in range(0, len(tochange)):
+                a=x//int(len(key))
+                y=int(x-(a*len(key)))
+                code= int(tochange[x]+key[y])
+                print(associations[code], end="")
+        print()
+    if mode == "d":
+        tochange = list(input("Message: "))
+        key = list(input("Key: "))
+        for x in range(0, len(tochange)):
+            tochange[x]=associations.find(tochange[x])
+        for x in range(0, len(key)):
+            key[x]=associations.find(key[x])
+        for x in range(0, len(tochange)):
+            a=x//int(len(key))
+            y=int(x-(a*len(key)))
+            code= int(tochange[x]-key[y])
+            print(associations[code], end="")
+        print()
