96237ff510aee43ea3894ffc2fd47b89f2425e6d
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-10-05 13:09:03 -0400

    Updated from Brython Server: 10/5/2016 1:09:02 PM

--- cryptography.py
+++ cryptography.py
@@ -23,7 +23,7 @@ while a!=1:
         tochange = list(input("Message: "))
         key = list(input("Key: "))
         for x in range(0, len(tochange)):
-            tochange[x]=associations.find(tochange[x])-(associations.find(tochange[x])//87)
+            tochange[x]=associations.find(tochange[x])-((associations.find(tochange[x])//87)*87)
             
         for x in range(0, len(key)):
             key[x]=associations.find(key[x])
